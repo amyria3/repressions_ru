4bcc51c53282f9bfe561dfe0e3e641ca44875d60
Georg Mühlenberg <georg.muehlenberg@jambit.com>
2022-05-19 01:01:29 +0200

    Translation preparations
    index.html
    - add translation keys everywhere
    
    translator.js
    - translate the alt attribute of IMG elements
    - log error on unable to load JSON translation file

--- JS/src/translator.js
+++ JS/src/translator.js
@@ -2,14 +2,13 @@
 
 class Translator {
 
-
     constructor() {
         this._lang = this.getLanguage();
         this._elements = document.querySelectorAll("[data-i18n]");
     }
 
     getLanguage() {
-        let lang = navigator.languages ? navigator.languages[0] : navigator.language;
+        const lang = navigator.languages ? navigator.languages[0] : navigator.language;
         return lang.substring(0, 2);
     }
 
@@ -18,25 +17,27 @@ class Translator {
         if (lang) {
             this._lang = lang;
         }
-        
+
         fetch("./JS/i18n/" + this._lang + ".json")
             .then((res) => res.json())
-            .then((translation) => {
-                this.translate(translation);
-            })
+            .then((translationFile) => this.translateElements(translationFile))
             .catch((e) => {
-                console.error("Could not load " + this._lang + ".json.");
+                console.error("Could not load " + this._lang + ".json: " + e);
             });
     }
 
-    translate(translation) {
-
+    translateElements(translationFile) {
         this._elements.forEach((element) => {
-            let keys = element.dataset.i18n.split(".");
-            let text = keys.reduce((obj, i) => obj[i], translation);
+            const text = element.dataset.i18n
+                .split(".")
+                .reduce((firstKey, secondKey) => firstKey[secondKey], translationFile);
 
             if (text) {
-                element.innerHTML = text;
+                if (element.tagName === "IMG") {
+                    element.alt = text;
+                } else {
+                    element.innerHTML = text;
+                }
             }
         })
     }
